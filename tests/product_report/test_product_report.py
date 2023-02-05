e34522a681fc08048b8db5f85da1cf5dc806c8a6
heitornobrega <nobheitor@gmail.com>
2023-02-05 00:45:18 -0300

    test: test of report make by Product class

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        id=1,
+        nome_do_produto="Cafe",
+        nome_da_empresa="Cafes Nature",
+        data_de_fabricacao="2020-07-04",
+        data_de_validade="2023-02-09",
+        numero_de_serie="FR48",
+        instrucoes_de_armazenamento="instrucao",
+    )
+    assert (
+        str(produto.__repr__())
+        == "O produto Cafe fabricado em 2020-07-04 por"
+        + " Cafes Nature com validade até 2023-02-09 precisa"
+        + " instrucao."
+    )
