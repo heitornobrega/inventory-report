4d487a42059582f3698707fd1df0756f12f7fbf2
heitornobrega <nobheitor@gmail.com>
2023-02-05 00:56:15 -0300

    fix: fix the XmlImporter class

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -5,13 +5,16 @@ import xml.etree.ElementTree as ET
 class XmlImporter(Importer):
     @classmethod
     def import_data(cls, src):
-        list_test = []
-        with open(src, "r") as file:
-            tree = ET.parse(file)
-            root = tree.getroot()
-        for element in root:
-            dict = {}
-            for child in element:
-                dict[child.tag] = child.text
-            list_test.append(dict)
-        return list_test
+        if "xml" in src:
+            list_test = []
+            with open(src, "r") as file:
+                tree = ET.parse(file)
+                root = tree.getroot()
+            for element in root:
+                dict = {}
+                for child in element:
+                    dict[child.tag] = child.text
+                list_test.append(dict)
+            return list_test
+        else:
+            raise ValueError("Arquivo inválido")
